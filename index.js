aecd5e6781c1b4b7f08286e1b3d779ce80565555
pranshunagar01 <59359937+pranshunagar01@users.noreply.github.com>
2021-06-29 21:53:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var audio1 = new Audio("sounds/sound1.mp3");
+var audio2 = new Audio("sounds/sound2.mp3");
+var audio3 = new Audio("sounds/sound3.mp3");
+var audio4 = new Audio("sounds/sound4.mp3");
+var audio5 = new Audio("sounds/sound5.mp3");
+var audio6 = new Audio("sounds/sound6.mp3");
+var audio7 = new Audio("sounds/sound7.mp3");
+
+document.querySelector(".btn1").addEventListener("mouseover", function (){audio1.play();});
+document.querySelector(".btn2").addEventListener("mouseover", function (){audio2.play();});
+document.querySelector(".btn3").addEventListener("mouseover", function (){audio3.play();});
+document.querySelector(".btn4").addEventListener("mouseover", function (){audio4.play();});
+document.querySelector(".btn5").addEventListener("mouseover", function (){audio5.play();});
+document.querySelector(".btn6").addEventListener("mouseover", function (){audio7.play();});
+document.querySelector(".btn7").addEventListener("mouseover", function (){audio6.play();});
